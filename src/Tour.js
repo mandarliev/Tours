00648b07a303300ee00eaf117eed7dcc81ab9ba2
Dimitar <dimitar.mandarliev@gmail.com>
2023-01-02 11:06:42 +0200

    Added the removeTour function

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Tour = ({id,image,info,price,name}) => {
+const Tour = ({id,image,info,price,name,removeTour}) => {
   const [readMore, setReadMore] = useState(false);
   return (
     <article className='single-tour'>
@@ -14,7 +14,7 @@ const Tour = ({id,image,info,price,name}) => {
           {readMore?info:`${info.substring(0,200)}...`}
           <button onClick={()=> setReadMore(!readMore)}>{readMore?'show less':'read more'}</button>
         </p>
-        <button className='delete-btn'>
+        <button className='delete-btn' onClick={() =>removeTour(id)}>
           not interested
         </button>
       </footer>
